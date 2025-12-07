73df7b9180739d4fe51ad45fbcce145e88e55601
Denis Fitzpatrick <denis.fitzpatrick@boystown.org>
2025-12-07 02:12:41 -0600

    change script url again

--- scripts/comment-config.js
+++ scripts/comment-config.js
@@ -1,6 +1,6 @@
 // Replace the placeholder URL with your published Google Apps Script web-app URL.
 // The script should proxy reads and writes to a Google Sheet (see README for details).
-export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/library/d/1BgVyoBlIPKN7h7ZD9A_vwkmmzMc-4tdjIp0cp9EPDAcfrriGSbGHg1ah/1";
+export const GOOGLE_SCRIPT_URL = "https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLjy2RActtBWxQbSSDbvFm3-seKwNptuMQKdAaqNrweqpGWLTkYoGSpIwj_XJcEWBJE0NdTiX8zUJfTyaF_77AEBKoybGzLuPUxs82dIdHcTOTFeHUjOXlbLQokGBWod1QzmQ3YhE8eiQ-xrP0V9rRF7WAWZz75DU9AlEm5eEPTcCw1hn09vxA4JBBa1uzrXjkgw5V6KTYBiZSA-IV8Cil_4D8zLLSi2I-xLyv3dcjCS1wFnsnMIlc3B3QeufA&lib=MGsm6HPPaDYnOsutR9PAX5n-IzeMn86mB";
 
 // Optional: flip to true to force localStorage-only comments while developing offline.
 export const USE_LOCAL_FALLBACK_ONLY = false;
